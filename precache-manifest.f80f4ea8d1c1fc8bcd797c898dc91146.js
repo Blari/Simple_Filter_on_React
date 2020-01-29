f1221309ba4e6adbd7647f8b537a1c7a8f8bd7e3
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-01-29 12:07:32 +0300

    Updates

---
+++ precache-manifest.f80f4ea8d1c1fc8bcd797c898dc91146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682387be6e19ab2caec53c8880493c4e",
+    "url": "https//blari.github.io/reactSearcPage/index.html"
+  },
+  {
+    "revision": "6e3ef9f1718100031e51",
+    "url": "https//blari.github.io/reactSearcPage/static/css/main.0ec27938.chunk.css"
+  },
+  {
+    "revision": "35fb934d7e7614616deb",
+    "url": "https//blari.github.io/reactSearcPage/static/js/2.aa6b2f6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https//blari.github.io/reactSearcPage/static/js/2.aa6b2f6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e3ef9f1718100031e51",
+    "url": "https//blari.github.io/reactSearcPage/static/js/main.355ac86b.chunk.js"
+  },
+  {
+    "revision": "db598a0de854d507d52f",
+    "url": "https//blari.github.io/reactSearcPage/static/js/runtime-main.12a039d5.js"
+  }
+]);
