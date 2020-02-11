82aab8b7f7ab793828ac26d010551ad019771d66
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-11 14:34:39 +0300

    Updates

---
+++ precache-manifest.400f6362b20056fef9a6ef24dbeeb766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fdcf41293256c2b6633758a6902ca4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "553343ad3307cb0b16cc",
+    "url": "./static/css/main.1881f3a5.chunk.css"
+  },
+  {
+    "revision": "35fb934d7e7614616deb",
+    "url": "./static/js/2.aa6b2f6b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.aa6b2f6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "553343ad3307cb0b16cc",
+    "url": "./static/js/main.1ab31d14.chunk.js"
+  },
+  {
+    "revision": "8c7cc4caf17cabe3543f",
+    "url": "./static/js/runtime-main.ed243b44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactSearcPage/precache-manifest.b7c85dfbb3c7d3f4c9314c20a9e48b18.js"
+  "./precache-manifest.400f6362b20056fef9a6ef24dbeeb766.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactSearcPage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
