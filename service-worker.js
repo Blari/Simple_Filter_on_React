ae5a5b306341f339bb990aae64775b99d8278e7a
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-11 13:10:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https//blari.github.io/reactSearcPage/precache-manifest.33d14534364a5ca6db278f721c296308.js"
+  "/reactSearcPage/precache-manifest.b7c85dfbb3c7d3f4c9314c20a9e48b18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//blari.github.io/reactSearcPage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactSearcPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
